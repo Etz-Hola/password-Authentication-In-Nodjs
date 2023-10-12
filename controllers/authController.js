144b3b15af87189f7c2810ed72f173b60e948fcf
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-12 10:50:40 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,5 +6,19 @@ const usersDB = {
 
 const bcrypt = require('bcrypt');
 const handleLogin = async (req, res) => {
-    const {...user, pwd} = req.body
-} 
+    const {user, pwd} = req.body
+    if(!user || !pwd) return res.status(400).json({"message": " in valid username and password"})
+    const foundUser = usersDB.users.find(person => person.username === user)
+    if(!foundUser) return res.sendStatus(401) // unauthorize
+
+    const match = await compare(pwd, foundUser.password)
+        if(match) {
+            res.json({"success": `user${user} is logged in!`})
+        }else {
+            res.sendStatus(401)
+        }
+
+}
+
+
+module.exports = {handleLogin}
