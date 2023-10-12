7997f474f244cf87e298b518928339ba541dedec
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-12 09:23:39 +0100

    update

---
+++ controllers/registerController.js
@@ -0,0 +1,36 @@
+const usersDB = {
+    users:require('../model/users.json'),
+    setUsers: function (data) {this.user = data},
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async(req, res) => {
+    const {user,pwd} = req.body;
+
+    if(!user || !pwd) return res.status(400).json({'message': 'Username and password must be provided required'});
+
+    try {
+        //encrypt the password
+        const hashedPwd = await bcrypt.hash(pwd, 10)
+
+        //store the new user
+        const newUser ={'username': user, "password": hashedPwd}
+
+        usersDB.setUsers([...usersDB.users, newUser])
+        await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'user.json'),
+        JSON.stringify(usersDB.users))
+
+        console.log(usersDB.users);
+        res.status(201).json({success: `New user ${user} created!`})
+
+
+    }catch (err) {
+        res.status(500).json({'message': err.message});
+    }    
+}
+
+
+module.exports = {handleNewUser}
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errHandler');
+const corsOptions = require('./config/corsOptions')
+const PORT = process.env.Port || 4100
+
+
+
+//buiting in  
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+
+//STATIC ROUTES
+app.use('/', express.static(path.join(__dirname, "public")));
+
+// ROUTES 
+app.use('/', require('./routes/root.js'))
+app.use('/register', require("./routes/register"));
+app.use('/employees', require('./routes/api/employees.js'))
+
+
+
+
+app.use(logger)
+
+
+app.use(cors(corsOptions));
+
+
+//app.all is the route handler for all requests
+app.all('/*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accept('json')) {
+        res.json({error: '404 Not found' });
+    }else {
+        res.type('txt').send('404 Not found')
+    }
+})
+
+
+app.use(errorHandler)
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+})
+
+
